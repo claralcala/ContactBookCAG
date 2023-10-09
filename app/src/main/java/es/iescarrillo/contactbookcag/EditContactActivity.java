d9c6c9693a6a06ec2725fbf323f050178ea2e2ca
Clara Alcalá García <calcgar312@iescarrillo.es>
2023-10-09 13:33:23 +0200

    changes

---
+++ app/src/main/java/es/iescarrillo/contactbookcag/EditContactActivity.java
@@ -0,0 +1,14 @@
+package es.iescarrillo.contactbookcag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_contact);
+    }
+}
---
+++ app/src/main/java/models/Contacto.java
@@ -0,0 +1,74 @@
+package models;
+
+import java.util.Objects;
+
+public class Contacto {
+
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String telephone;
+
+
+    public Contacto(String name, String surname, String email, String telephone){
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.telephone = telephone;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contacto contacto = (Contacto) o;
+        return Objects.equals(name, contacto.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+}
